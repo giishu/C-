2cfe16be5d34a8e710f3dddb3e7fc881591afe19
Nicolás Diaz <nicolasdiaz13ips@gmail.com>
2025-04-15 16:12:25 -0300

    Vehiculos

---
+++ Vehiculos/Vehiculos/Vehiculos/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Vehiculos
+{
+    interface IVehiculos
+    {
+        int mover(int tiempo);
+        void posicion();
+        void reiniciarPosicion();
+    }
+
+    public class Bicicletas : IVehiculos
+    {
+        public const int limite = 10;
+        public int pos_act = 0;
+
+        public int mover(int tiempo)
+        {
+            pos_act += tiempo * limite;
+            return tiempo * limite;
+        }
+
+        public void posicion()
+        {
+            Console.WriteLine(pos_act);
+        }
+
+        public void reiniciarPosicion()
+        {
+            pos_act = 0;
+        }
+    }
+
+    public class Camiones : IVehiculos
+    {
+        public const int limite = 30;
+        public int pos_act = 0;
+
+        public int mover(int tiempo)
+        {
+            pos_act += tiempo * limite;
+            return tiempo * limite;
+        }
+
+        public void posicion()
+        {
+            Console.WriteLine(pos_act);
+        }
+
+        public void reiniciarPosicion()
+        {
+            pos_act = 0;
+        }
+    }
+
+    public class Auto : IVehiculos
+    {
+        public int pos_act = 0;
+        public int limite;
+
+        public Auto(int v_max)
+        {
+            limite = v_max;
+        }
+
+        public int mover(int tiempo)
+        {
+            pos_act += tiempo * limite;
+            return tiempo * limite;
+        }
+
+        public void posicion()
+        {
+            Console.WriteLine(pos_act);
+        }
+
+        public void reiniciarPosicion()
+        {
+            pos_act = 0;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
