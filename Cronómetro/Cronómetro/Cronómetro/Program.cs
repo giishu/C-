bb9b59d7458ab689d2542a71fa20d482e0ef64f0
unknown <nicolasdiaz13ips@gmail.com>
2025-04-15 01:20:41 -0300

    Cronometro

---
+++ Cronómetro/Cronómetro/Cronómetro/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Cronómetro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cronometro cronometro = new Cronometro();
+            for(int i = 0; i < 10; i++)
+            {
+                cronometro.incrementar_Tiempo();
+            }
+            cronometro.mostrar_Tiempo();
+        }
+    }
+
+    public class Cronometro
+    {
+        public int segundos = 0;
+        public int minutos = 0;
+
+        public void incrementar_Tiempo()
+        {
+            segundos++;
+            if (segundos == 60)
+            {
+                segundos = 0;
+                minutos++;
+            }   
+        }
+        public void reiniciar_Tiempo()
+        {
+            segundos = 0;
+            minutos = 0;
+        }
+        public void mostrar_Tiempo()
+        {
+            Console.WriteLine("Tiempo: " + minutos + ":" + segundos);
+        }
+    }
+}
